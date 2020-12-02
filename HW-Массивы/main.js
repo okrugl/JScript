5915f206fbb2452218a5e2299d3baab000c2e02f
okrugl <okrughelen@gmail.com>
2020-12-02 18:07:45 +0300

    Домашка /часть/ массивы

---
+++ HW-Массивы/main.js
@@ -0,0 +1,50 @@
+
+
+
+// //1--Создайте массив и в цикле заполните его чётными числами от 2 до 20.
+// var arr = [];
+
+// for(var x = 2; x <= 20; x++){
+//     if(x % 2 == 0){
+//         arr.push(x);
+//     }
+// }
+
+// console.log(arr);
+
+// //2--  в этом массиве каждый элемент умножить на 5
+
+// for(var i = 0; i < arr.length; i++){
+//     arr[i] = i * 5;
+// }
+
+// console.log(arr); 
+
+// //3--  расположить элементы из задачи 2 в обратном порядке
+
+// for(var i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
+
+var elemMass = [];
+var elemMass1 = +prompt('Ведите перое число');
+var elemMass2 = +prompt('Ведите второе число');
+var elemMass3 = +prompt('Ведите третье число');
+
+var elemMass = [elemMass1 + ', ' + elemMass2 + ', ' + elemMass3];
+console.log(elemMass);
+
+for(var x = 0; x < 0; x++){
+    if(elemMass[0] > elemMass[1] || elemMass[2]){
+        console.log(elemMass[0] / elemMass[1] && elemMass[2]);
+    }
+else{
+   console.log(elemMass); 
+}
+}
+// for(var x = 0; x <= elemMass; x++){
+//     if(x > elemMass[0]){
+//         console.log(x / (elenMass[0]));
+//     }
+
+// }
